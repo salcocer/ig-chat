b363615a340b15b3287dd919d6ccc887e343d13f
Alejandro Alcocer <saavalencia97@gmail.com>
2026-03-17 11:11:49 +0700

    init schdn ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { ModeToggle } from "./components/mode-toggle";
 import "./global.css";
 
 function App() {
   return (
     <div className="bg-color w-90 h-150 p-4 bg-gray-color">
       <button className="text-color">Switch to "light" : "dark"</button>
+      <ModeToggle />
     </div>
   );
 }
