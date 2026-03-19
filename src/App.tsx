40841afe6d6618ca18b285053c3e413be263f8c0
Alejandro Alcocer <saavalencia97@gmail.com>
2026-03-19 17:16:33 +0700

    vip

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./global.css";
 
 function App() {
   return (
-    <div className="bg-color w-84 h-140 p-4 rounded-2xl">
+    <div className="bg-color w-84 h-140 p-4">
       <ModeToggle />
     </div>
   );
